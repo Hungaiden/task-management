2e18bd3fcff385e65628e0e4572389d17c5463ac
Bui Duc Hung <duchungbui1@gmail.com>
2024-11-01 22:23:22 +0700

    1. Phan trang

--- controllers/client/task.controller.js
+++ controllers/client/task.controller.js
@@ -14,10 +14,32 @@ module.exports.index = async (req, res) => {
   if(req.query.sortKey && req.query.sortValue) {
     sort[req.query.sortKey] = req.query.sortValue;
   }
-  const tasks = await Task.find(find).sort(sort);
+  // End sort
+
+
+   // Phân trang
+  let limitItems = 4;
+  let page = 1;
+  if(req.query.page) {
+    page = parseInt(req.query.page);
+  }
+  if(req.query.limit) {
+    limitItems = parseInt(req.query.limit);
+  }
+  const skip = (page - 1) * limitItems;
+  // Hết Phân trang
+
+  
+  const tasks = await Task
+    .find(find)
+    .limit(limitItems)
+    .skip(skip)
+    .sort(sort);
+
   
   res.json(tasks);
 }
+
 
 module.exports.detail = async (req, res) => {
   const id = req.params.id;
