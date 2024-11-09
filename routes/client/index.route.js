268f3d128db6e9a0ddd12d10b0de8f872a46145c
Bui Duc Hung <duchungbui1@gmail.com>
2024-11-09 22:35:53 +0700

    11.Tinh nang dang ky

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,6 +1,8 @@
 const tasksRoute = require("./task.route");
 
+const usersRoute = require("./user.route");
 module.exports = (app) => {
   app.use("/tasks", tasksRoute);
   
+  app.use("/users", usersRoute );
 }
