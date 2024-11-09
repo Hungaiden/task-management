672bbb8e33526cdc566f6a37b9dd0b4ee5da4be5
Bui Duc Hung <duchungbui1@gmail.com>
2024-11-09 22:04:15 +0700

    9.Cai dat cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require('dotenv').config();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
@@ -10,6 +11,14 @@ databse.connect();
 
 const routeClient = require("./routes/client/index.route");
 
+ // Tất cả tên miền được phép truy cập vào
+app.use(cors());
+// Cho phép 1 tên miền cụ thể được phép truy cập
+// const corsOptions = {
+//   origin: 'http://example.com',
+//   optionsSuccessStatus: 200
+// }
+// cors(corsOptions);
 // parse application/json
 app.use(bodyParser.json());
 
