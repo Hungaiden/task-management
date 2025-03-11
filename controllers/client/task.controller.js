9adcb03c7638a625d54daa08c1980bf20d361bdd
Bui Duc Hung <duchungbui1@gmail.com>
2025-03-11 11:34:15 +0700

    Them id cua nguoi them va sua

--- controllers/client/task.controller.js
+++ controllers/client/task.controller.js
@@ -1,49 +1,49 @@
 const Task = require("../../models/task.model");
 
 module.exports.index = async (req, res) => {
-  const find = { 
+  const find = {
     deleted: false
   };
 
-  if(req.query.status) {
+  if (req.query.status) {
     find.status = req.query.status;
   }
 
   //SORT
   const sort = {};
-  if(req.query.sortKey && req.query.sortValue) {
+  if (req.query.sortKey && req.query.sortValue) {
     sort[req.query.sortKey] = req.query.sortValue;
   }
   // End sort
 
 
-   // Phân trang
+  // Phân trang
   let limitItems = 4; // neu k gui len thi de mac dinh nhu nay
   let page = 1;
-  if(req.query.page) {
+  if (req.query.page) {
     page = parseInt(req.query.page);
   }
-  if(req.query.limit) {
+  if (req.query.limit) {
     limitItems = parseInt(req.query.limit);
   }
   const skip = (page - 1) * limitItems;
   // Hết Phân trang
 
   // Tìm kiếm
-  if(req.query.keyword) {
+  if (req.query.keyword) {
     const regex = new RegExp(req.query.keyword, "i");
     find.title = regex;
   }
   // Hết Tìm kiếm
 
-  
+
   const tasks = await Task
     .find(find)
     .limit(limitItems)
     .skip(skip)
     .sort(sort);
 
-  
+
   res.json(tasks);
 }
 
@@ -61,9 +61,11 @@ module.exports.changeMultiPatch = async (req, res) => {
   const status = req.body.status; // nhung thu vien body parser vao moi dung body dc
   const ids = req.body.ids;
 
-  if(status) {
+  if (status) {
     const tasks = await Task.updateMany({
-      _id : {$in : ids} // cap nhat cac id trong mang ids
+      _id: {
+        $in: ids
+      } // cap nhat cac id trong mang ids
     }, {
       status: status
     })
@@ -77,8 +79,9 @@ module.exports.changeMultiPatch = async (req, res) => {
 
 module.exports.createPost = async (req, res) => {
   const data = req.body;
-
+  data.createdBy = req.user.id;
   const task = new Task(data);
+
   await task.save();
 
   res.json({
@@ -91,6 +94,7 @@ module.exports.createPost = async (req, res) => {
 module.exports.editPatch = async (req, res) => {
   const id = req.params.id;
   const data = req.body;
+  data.updatedBy = req.user.id;
 
   await Task.updateOne({
     _id: id
@@ -102,15 +106,17 @@ module.exports.editPatch = async (req, res) => {
   })
 }
 
-module.exports.deleteMultiPatch = async(req, res) => {
+module.exports.deleteMultiPatch = async (req, res) => {
   const ids = req.body.ids;
 
   await Task.updateMany({
-    _id: { $in: ids }
+    _id: {
+      $in: ids
+    }
   }, {
     deleted: true
   });
-  
+
   res.json({
     code: "success",
     message: "Xóa thành công!"
--- models/task.model.js
+++ models/task.model.js
@@ -7,6 +7,8 @@ const taskSchema = new mongoose.Schema(
     content: String,
     timeStart: Date,
     timeFinish: Date,
+    createdBy: String,
+    updatedBy: String,
     deleted: {
       type: Boolean,
       default: false,
